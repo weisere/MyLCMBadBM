562810b7894d7947b7e8494b07f05b9be31654bb
Elyah Weiser <eweiser@student.touro.edu>
2023-03-17 14:43:27 -0400

    DiskWorker: added An UIInterface variable and a setter method setUI() which will allow The App class to set which UI DiskWorker will use

--- src/edu/touro/mco152/bm/DiskWorker.java
+++ src/edu/touro/mco152/bm/DiskWorker.java
@@ -39,7 +39,11 @@ import static edu.touro.mco152.bm.DiskMark.MarkType.WRITE;
 
 public class DiskWorker /*extends SwingWorker<Boolean, DiskMark>*/ {
 
-    //UIInterface ui;
+    UIInterface ui;
+
+    public void setUI(UIInterface ui){
+        this.ui = ui;
+    }
 
 
 
