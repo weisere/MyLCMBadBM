c2b0f6d01058fab0edd4151763eae0e96a073357
Elyah Weiser <eweiser@student.touro.edu>
2023-03-16 22:35:59 -0400

    DiskWorker: removed the SwingWorker implementation. Edited the doInBackground() methods name and is no longer inherited from SwingWorker. Removed the process() and done() methods, both being swing methods.

--- src/edu/touro/mco152/bm/DiskWorker.java
+++ src/edu/touro/mco152/bm/DiskWorker.java
@@ -37,13 +37,14 @@ import static edu.touro.mco152.bm.DiskMark.MarkType.WRITE;
  * Swing using an instance of the DiskMark class.
  */
 
-public class DiskWorker extends SwingWorker<Boolean, DiskMark> {
+public class DiskWorker /*extends SwingWorker<Boolean, DiskMark>*/ {
 
-    // Record any success or failure status returned from SwingWorker (might be us or super)
-    Boolean lastStatus = null;  // so far unknown
+    //UIInterface ui;
 
-    @Override
-    protected Boolean doInBackground() throws Exception {
+
+
+    //@Override
+    protected static Boolean _doInBackground() throws Exception {
 
         /*
           We 'got here' because: 1: End-user clicked 'Start' on the benchmark UI,
@@ -287,41 +288,5 @@ public class DiskWorker extends SwingWorker<Boolean, DiskMark> {
         return true;
     }
 
-    /**
-     * Process a list of 'chunks' that have been processed, ie that our thread has previously
-     * published to Swing. For my info, watch Professor Cohen's video -
-     * Module_6_RefactorBadBM Swing_DiskWorker_Tutorial.mp4
-     * @param markList a list of DiskMark objects reflecting some completed benchmarks
-     */
-    @Override
-    protected void process(List<DiskMark> markList) {
-        markList.stream().forEach((dm) -> {
-            if (dm.type == DiskMark.MarkType.WRITE) {
-                Gui.addWriteMark(dm);
-            } else {
-                Gui.addReadMark(dm);
-            }
-        });
-    }
 
-
-    @Override
-    protected void done() {
-        // Obtain final status, might from doInBackground ret value, or SwingWorker error
-        try {
-            lastStatus = super.get();   // record for future access
-        } catch (Exception e) {
-            Logger.getLogger(App.class.getName()).warning("Problem obtaining final status: " + e.getMessage());
-        }
-
-        if (App.autoRemoveData) {
-            Util.deleteDirectory(dataDir);
-        }
-        App.state = App.State.IDLE_STATE;
-        Gui.mainFrame.adjustSensitivity();
-    }
-
-    public Boolean getLastStatus() {
-        return lastStatus;
-    }
 }
