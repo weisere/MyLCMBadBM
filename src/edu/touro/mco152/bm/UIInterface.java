069fa342a63e749fbd23f257e33d85af65d920e0
Elyah Weiser <eweiser@student.touro.edu>
2023-03-16 21:46:16 -0400

    Created anUI interfaced called UIInterface.java that contains the 3 methods that App needs from the UI and the 3 methods that DiskWorker needs from the UI

---
+++ src/edu/touro/mco152/bm/UIInterface.java
@@ -0,0 +1,16 @@
+package edu.touro.mco152.bm;
+
+public interface UIInterface {
+
+    public void _cancel();
+
+    public void _addPropertyChangeListener();
+
+    public void _execute();
+
+    public boolean _isCancelled();
+
+    public void _setProgress();
+
+    public void _publish();
+}
