757404e59ea937b910e9d7be4f0fe5d34b798c50
Elyah Weiser <eweiser@student.touro.edu>
2023-03-16 23:09:32 -0400

    UIInterface: added the parameter variables that will need to be passed into the methods that a UI will implement

--- src/edu/touro/mco152/bm/UIInterface.java
+++ src/edu/touro/mco152/bm/UIInterface.java
@@ -1,5 +1,7 @@
 package edu.touro.mco152.bm;
 
+import java.beans.PropertyChangeEvent;
+
 /**
  * This is an interface that will contain all methods that the App class
  * and DiskWorker class will need from a UI in order to display benchmark
@@ -8,15 +10,15 @@ package edu.touro.mco152.bm;
 public interface UIInterface {
 
 
-    public void _cancel();
+    public void _cancel(boolean b);
 
-    public void _addPropertyChangeListener();
+    public void _addPropertyChangeListener(PropertyChangeEvent event);
 
     public void _execute();
 
     public boolean _isCancelled();
 
-    public void _progress();
+    public void _setProgress(int percentComplete);
 
-    public void _publish();
+    public void _publish(DiskMark wMark);
 }
